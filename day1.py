59798baba2866b9bef8354b51cc1447a1318881b
Ali <ali.dasilva@lernende.ethz.ch>
2021-02-19 11:13:37 +0100

    ADDING Working challenge day1.py

---
+++ day1.py
@@ -0,0 +1,24 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Declare second integer, double, and String variables.
+# Read and save an integer, double, and String to your variables.
+
+#TODO For it to work on Hackerrank take out the 3 print statements below
+print("Please input an int")
+o = int(input())
+print("Please input a float")
+f = float(input())
+print("Please input a string")
+a = str(input())
+
+# Print the sum of both integer variables on a new line.
+print(i + o)
+
+# Print the sum of the double variables on a new line.
+print(d + f)
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + a)
